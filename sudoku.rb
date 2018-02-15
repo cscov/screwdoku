de978be9a0bdb43b90a318015fbecb7c2a0134c6
Jay Johnson <jaysbeverage@gmail.com>
2018-02-15 14:31:04 -0800

    swapped valid_pos? with valid_val? in get_pos and get_val methods also swapped pos and val variable assignments with get_val and get_pos to correct ones

--- sudoku.rb
+++ sudoku.rb
@@ -1,4 +1,5 @@
 require_relative "board"
+require 'byebug'
 
 class SudokuGame
   def self.from_file(filename)
@@ -12,22 +13,23 @@ class SudokuGame
 
   def get_pos
     pos = nil
-    until pos && valid_val?(pos)
+    # debugger
+    until pos && valid_pos?(pos)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
-      pos = parse_val(gets.chomp)
+      pos = parse_pos(gets.chomp)
     end
     pos
   end
 
   def get_val
     val = nil
-    until val && valid_pos?(val)
+    until val && valid_val?(val)
       puts "Please enter a position on the board (e.g., '3,4')"
       print "> "
 
       begin
-        val = parse_pos(gets.chomp)
+        val = parse_val(gets.chomp)
       rescue
         puts "Invalid position entered (did you use a comma?)"
         puts ""
@@ -48,9 +50,10 @@ class SudokuGame
 
   def play_turn
     board.render
-
-    val = get_val
+    # debugger
     pos = get_pos
+    val = get_val
+
 
     board[pos] = val
   end
